b162d914977a496ee9fd8dd41ab5179bab00bc75
Leo <Ditlev110@hotmail.com>
2025-04-10 17:33:31 +0200

    Scrolling of the MindeStamme done!
    Scales with any amount of previous owners! there is a few things to optimize,  such as some of the hard coding, but that is it!

--- Assets/Scripts/Scroll.cs
+++ Assets/Scripts/Scroll.cs
@@ -1,10 +1,9 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
-
-//interface ImindeStamme : MonoBehaviour
-//{
-    
-//}
+using UnityEngine.UI;
 
 
 
@@ -15,64 +14,156 @@ public class Scroll : MonoBehaviour
     public GameObject MSStammePrefab;
     public GameObject MSMindeBtn;
     public GameObject MSScrollContent;
+    List<GameObject> treeTrunks = new List<GameObject>();
+    float HeightDifference;
+    public float cropB;
+    public float cropT;
+
+    private void Start()
+    {
+        AddTrunk();
+    }
+
 
     void AddTrunk()
     {
-        ejerPerScroll = 4;
-        int ejerCount = tidligereEjer % ejerPerScroll;
-        float pagescrolls = tidligereEjer / ejerPerScroll;
+        ejerPerScroll = 4; // amount of people per page (roughly)
+        int ejerCount = tidligereEjer % ejerPerScroll; // the number of spialge of previous owners onto the next trunk / scroll 
+        float pagescrolls = Mathf.Ceil(tidligereEjer / ejerPerScroll); // how many scrols is there needed to make to scroll to the top, we round up to a whole number because we can crop the rest later. -1 because the first page is free >:3
+        
+        //float TVal;
 
-        CropTrunk(ejerCount);
         for (int i = 0; i < pagescrolls; i++)
         {
-            float TVal = (i * MSStammePrefab.GetComponent<RectTransform>().sizeDelta.y);
-            Instantiate(MSStammePrefab, new Vector2 (0, TVal), new Quaternion(),MSScrollContent.transform);
-            MSScrollContent.transform.localPosition.y = TVal; MSScrollContent.transform.localScale.y = TVal;
+            // Tval is the amount of units that the new trunk will do
+            
+            //Quaternion rotation = Quaternion.Euler(0, 0, 0);
+
+            //instantiate the prefab so it is a child of content, and offset in the y coordinate
+            //Instantiate(MSStammePrefab, position, rotation, MSScrollContent.transform);
+            //Instantiate(MSStammePrefab);
+            //MSStammePrefab.transform.parent = MSScrollContent.transform;
+            //MSStammePrefab.GetComponent<RectTransform>().position = new Vector2(MSStammePrefab.GetComponent<RectTransform>().position.x, TVal);
+
+            
+            
+
+            GameObject Stamme = (GameObject)Instantiate(MSStammePrefab, MSScrollContent.transform, false);
+
+
+            //MSStammePrefab.transform.SetParent(MSScrollContent.transform, false);
+            //float TVal = (N * (MSStammePrefab.GetComponent<RectTransform>().sizeDelta.y / 2)) + 450;
+           
+            
+
+            treeTrunks.Add(Stamme);
+            //treeTrunks.LastOrDefault();
+
+            //Makes the GameObject "newParent" the parent of the GameObject "player".
+            //player.transform.parent = newParent.transform;
+
+            //MSScrollContent.transform.localPosition.y = TVal; MSScrollContent.transform.localScale.y = TVal;
 
 
             //GameObject NewObj = Instantiate(MSStammePrefab);
             //NewObj.transform.parent = MSScrollContent.transform;
             //NewObj.transform.localPosition = (i * NewObj.GetComponent<RectTransform>().sizeDelta.y);
         }
-        
+
+        int N = 2;
+        foreach (GameObject Trunk in treeTrunks)
+        {
+            float TVal = (N * Trunk.GetComponent<RectTransform>().sizeDelta.y);
+                //-(1780.5f - (MSStammePrefab.GetComponent<RectTransform>().anchoredPosition.y + (MSStammePrefab.GetComponent<RectTransform>().sizeDelta.y/2)));
+            
+            //Vector2 position = new Vector3(MSStammePrefab.GetComponent<RectTransform>().position.x, TVal);
+            
+            N += 1;
+            Vector2 position = new Vector3(0, TVal);
+            Trunk.GetComponent<RectTransform>().anchoredPosition = position;
+            //Debug.Log("TVal is : " + TVal);
+            //Debug.Log("Position is : " + position);
+            //Debug.Log("And the Trunk is at : " + Trunk.GetComponent<RectTransform>().position);
+
+
+        }
+
         
 
+        // position of the last tree trunk! 747 is the hight that we want between the trunk and top of the content edge
+        HeightDifference = (treeTrunks.Last().GetComponent<RectTransform>().sizeDelta.y / 2) + treeTrunks.Last().GetComponent<RectTransform>().anchoredPosition.y + 747;
+        Debug.Log(HeightDifference);
+        
+        //we want to crop the last truck being made
+        CropTrunk(ejerCount);
+
+        // we want the content container to change its height with the content within i.e. the trunk.
+        MSScrollContent.GetComponent<RectTransform>().sizeDelta = new Vector2(MSScrollContent.GetComponent<RectTransform>().anchoredPosition.x, HeightDifference);
+
+        // offset the position
+        MSScrollContent.GetComponent<RectTransform>().anchoredPosition = new Vector2(MSScrollContent.GetComponent<RectTransform>().anchoredPosition.x, HeightDifference + MSScrollContent.GetComponent<RectTransform>().anchoredPosition.y);
+
+
+        //string Str = "There is :";
+        //foreach (GameObject s in treeTrunks)
+        //{
+        //    Str = Str + $"{s}, ";
+        //}
+        //Debug.Log(Str);
+
     }
-    void CropTrunk(int ejerCount)
+    void CropTrunk(int count)
     {
-        if (ejerCount == 0)
+        if (count == 0)
         {
             return;
         }
-        else
+        else if (count == 1)
         {
+            Crop(cropB = 0.20f, cropT = 0.40f);
+            HeightDifference -= MSStammePrefab.GetComponent<RectTransform>().sizeDelta.y * (1-cropT);
+        }
+        else if (count == 2)
+        {
+            Crop(cropB = 0.50f, cropT = 0.50f);
+            HeightDifference -= MSStammePrefab.GetComponent<RectTransform>().sizeDelta.y * (1-cropT);
+        }
+        else if (count == 3)
+        {
+            Crop(cropB = 0.60f, cropT = 0.80f);
+            HeightDifference -= MSStammePrefab.GetComponent<RectTransform>().sizeDelta.y * (1-cropT);
             //---Dependent on ejerCount---\\
             //CROP N.NN of branch and N.NN of Trunk!
             //ADD the rest of the Bottons!
         }
     }
-
+    void Crop(float a, float b)
+    {
+        treeTrunks.Last().GetComponent<Image>().fillAmount = a;
+        treeTrunks.Last().transform.GetChild(0).GetComponent<Image>().fillAmount = b;
+        // treeTrunks.Last().GetComponentsInChildren<Image>().fillAmount = b;
+    }
     //foreach (var Log in ejerLog)
     //{
 
     //}
-    public class Wall : MonoBehaviour
-    {
-        public GameObject block;
-        public int width = 10;
-        public int height = 4;
+    //public class Wall : MonoBehaviour
+    //{
+    //    public GameObject block;
+    //    public int width = 10;
+    //    public int height = 4;
 
-        void Start()
-        {
-            for (int y = 0; y < height; ++y)
-            {
-                for (int x = 0; x < width; ++x)
-                {
-                    Instantiate(block, new Vector3(x, y, 0), Quaternion.identity);
-                }
-            }
-        }
-    }
+    //    void Start()
+    //    {
+    //        for (int y = 0; y < height; ++y)
+    //        {
+    //            for (int x = 0; x < width; ++x)
+    //            {
+    //                Instantiate(block, new Vector3(x, y, 0), Quaternion.identity);
+    //            }
+    //        }
+    //    }
+    //}
 
 
 
