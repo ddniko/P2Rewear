14edc08b8adaf6cca28665bf408298b766f87d6e
ddniko <31697075+ddniko@users.noreply.github.com>
2025-04-15 14:05:50 +0200

    Login done

---
+++ Assets/NBK-Singuplogin/LogIn.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using TMPro;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class LogIn : MonoBehaviour
+{
+
+    public TMP_InputField Brugernavn;
+    public TMP_InputField Password;
+    public GameObject Bottombar;
+
+    private List<MParent> mParents;
+
+
+    private void Awake()
+    {
+        DBManager.Init();
+        //mParents = DBManager.GetAllParents();
+    }
+    
+
+    public void Login()
+    {
+        mParents = DBManager.GetAllParents();
+        MParent loggedInParent = mParents.Find(p => p.Name == Brugernavn.text && p.Password == Password.text);
+        if (loggedInParent != null)
+        {
+            Bottombar.SetActive(true);
+            gameObject.SetActive(false);
+        }
+
+    }
+}
